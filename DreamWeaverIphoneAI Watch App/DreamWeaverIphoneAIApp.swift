c67c869311e4ebd69f050b222e4f5d775d8bbc36
Jozsef Gazsik <Jozsef.Gazsik@erstegroup.com>
2025-11-09 23:54:33 +0100

    new watch to try

---
+++ DreamWeaverIphoneAI Watch App/DreamWeaverIphoneAIApp.swift
@@ -0,0 +1,17 @@
+//
+//  DreamWeaverIphoneAIApp.swift
+//  DreamWeaverIphoneAI Watch App
+//
+//  Created by Gazsik Jozsef 6035 ED on 09.11.2025.
+//
+
+import SwiftUI
+
+@main
+struct DreamWeaverIphoneAI_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ DreamWeaverIphoneAI Watch AppTests/DreamWeaverIphoneAI_Watch_AppTests.swift
@@ -0,0 +1,17 @@
+//
+//  DreamWeaverIphoneAI_Watch_AppTests.swift
+//  DreamWeaverIphoneAI Watch AppTests
+//
+//  Created by Gazsik Jozsef 6035 ED on 09.11.2025.
+//
+
+import Testing
+@testable import DreamWeaverIphoneAI_Watch_App
+
+struct DreamWeaverIphoneAI_Watch_AppTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
